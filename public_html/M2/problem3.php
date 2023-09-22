df98fe123a1ab22958b4d823c4e596a06a02ffe2
iarivera <iar3@njit.edu>
2023-09-21 21:51:44 -0400

    need to finish 2 and 3

--- public_html/M2/problem3.php
+++ public_html/M2/problem3.php
@@ -10,13 +10,13 @@ function bePositive($arr) {
     //note: use the $arr variable, don't directly touch $a1-$a4
     //TODO use echo to output all of the values as positive (even if they were 
     //originally positive) and maintain the original datatype
-    for ($i = 0; $i <= count($arr); $i++) {
+    for ($i = 0; $i < count($arr); $i++) {
         if (is_numeric($i)) {
             echo abs($arr[$i]) . "<br>";
         }
         else if (is_string($i)) {
             if(strpos($i, "-") !== true) {
-                echo [strlen($arr[$i - 1])] . "<br>";
+                echo [strlen($arr[$i])] . "<br>";
             }
         }
     }
