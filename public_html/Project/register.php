a601641cda252ab985cd00e935107a39c44b0510
iarivera <iar3@njit.edu>
2023-10-10 12:35:22 -0400

    in class activities

--- public_html/Project/register.php
+++ public_html/Project/register.php
@@ -1,3 +1,6 @@
+<?php
+require(__DIR__ . "/../../lib/functions.php");
+?>
 <form onsubmit="return validate(this)" method="POST">
     <div>
         <label for="email">Email</label>
@@ -23,4 +26,41 @@
 </script>
 <?php
  //TODO 2: add PHP Code
+ if(isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["confirm"])) {
+    $email = se($_POST,"email", "", false);
+    $password = se($_POST,"password", "", false);
+    $confirm = se($_POST,"confirm", "", false);
+    //TODO 3: Validation
+    $hasError = false;
+    if (empty($email)) {
+        echo "Email must not be empty";
+        $hasError = true;
+    }
+    //sanitize
+    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    //validate
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "Invalid email address";
+        $hasError = true;
+    }
+    if (empty($password)) {
+        echo "Password must not be empty";
+        $hasError = true;
+    }
+    if (empty($confirm)) {
+        echo "Confirm must not be empty";
+        $hasError = true;
+    }
+    if (strlen($password) < 8) {
+        echo "Password too short";
+        $hasError = true;
+    }
+    if(strlen($password) > 0 && $password !== $confirm) {
+        echo "Passwords must match";
+        $hasError = true;
+    }
+    if (!$hasError) {
+        echo "Welcome, $email";
+    }
+}
 ?>
