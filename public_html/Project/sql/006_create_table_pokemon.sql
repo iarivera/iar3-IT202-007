614c6a6dbbd3f6f1a95ae463321d094ade7daecc
iarivera <iar3@njit.edu>
2023-11-21 14:04:41 -0500

    edits to table creation

--- public_html/Project/sql/006_create_table_pokemon.sql
+++ public_html/Project/sql/006_create_table_pokemon.sql
@@ -1,1 +1,11 @@
 TEXT COMMENT 'Saving API Data into Database'
+CREATE TABLE CA_Pokemon(
+    `id`    int auto_increment not null,
+    `api_id` VARCHAR(10),
+    `name` VARCHAR(30),
+    `created`   timestamp default current_timestamp,
+    `modified`  timestamp default current_timestamp on update current_timestamp,
+    PRIMARY KEY(`id`),
+    UNIQUE KEY(`api_id`),
+    UNIQUE KEY(`name`)
+)
