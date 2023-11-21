6698ff1424709966badabc55ab928bd6dc501f4f
iarivera <iar3@njit.edu>
2023-11-21 14:17:40 -0500

    finished insert into db function

--- public_html/Project/admin/manage_pokemon_data.php
+++ public_html/Project/admin/manage_pokemon_data.php
@@ -1,14 +1,16 @@
-//Intro to API
 <?php
+// Intro to API
+// note we need to go up 1 more directory
 require(__DIR__ . "/../../partials/nav.php");
 
 if (!has_role("Admin")) {
     flash("You don't have permission to view this page", "warning");
     die(header("Location: " . get_url("home.php")));
 }
-
+//TODO need to update insert_breeds... to use the $mappings array and not go based on is_int for value
 function insert_pokemon_into_db($db, $pokemon, $mappings)
 {
+    // Prepare SQL query
     $query = "INSERT INTO `CA_Pokemon` ";
     if (count($pokemon) > 0) {
         $cols = array_keys($pokemon[0]);
@@ -16,6 +18,7 @@ function insert_pokemon_into_db($db, $pokemon, $mappings)
             return "`$col`";
         }, $cols)) . ") VALUES ";
 
+        // Generate the VALUES clause for each mon
         $values = [];
         foreach ($pokemon as $i => $mon) { //I am using mon as the singular for pokemon
             $pokemonPlaceholders = array_map(function ($v) use ($i) {
@@ -42,8 +45,24 @@ function insert_pokemon_into_db($db, $pokemon, $mappings)
             foreach ($cols as $col) {
                 $placeholder = ":$col$i";
                 $val = isset($pokemon[$col]) ? $pokemon[$col] : "";
+                $param = PDO::PARAM_STR;
+                if (str_contains($mappings[$col], "int")) {
+                    $param = PDO::PARAM_INT;
+                }
+                $stmt->bindValue($placeholder, $val, $param);
             }
         }
 
+        // Execute the statement
+        try {
+            $stmt->execute();
+        } catch (PDOException $e) {
+            error_log(var_export($e, true));
+        }
     }
+}
+
+function process_single_mon($mon, $columns, $mappings)
+{
+    // Process mon data
 }
