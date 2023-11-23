5c2e13f2d9567599094691a3fefec9620d39f820
iarivera <iar3@njit.edu>
2023-11-22 23:24:28 -0500

    attempt at getting tables in order

--- public_html/Project/sql/006_create_table_pokemon_stats.sql
+++ public_html/Project/sql/006_create_table_pokemon_stats.sql
@@ -1,7 +1,8 @@
-CREATE TABLE CA_Pokemon(
+CREATE TABLE CA_Pokemon_Stats(
     `id`    int auto_increment not null,
     `api_id` VARCHAR(10),
     `name` VARCHAR(30),
+    `rarity` VARCHAR(30),
     `created`   timestamp default current_timestamp,
     `modified`  timestamp default current_timestamp on update current_timestamp,
     PRIMARY KEY(`id`),
