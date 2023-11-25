9b7a245617da5e495d272383133e8b7db6fe3f1c
iarivera <iar3@njit.edu>
2023-11-25 17:06:52 -0500

    added placeholder form

--- partials/search_form.php
+++ partials/search_form.php
@@ -37,3 +37,46 @@ $orders = array_map(function ($v) {
 }, $orders);
 array_unshift($orders, ["label" => "Any", "value" => ""]);
 ?>
+
+<form method="GET">
+    <div class="row">
+        <div class="col-auto">
+            <?php render_input(["type" => "text", "id" => "name", "name" => "name", "label" => "Name", "value" => se($search, "name", "", false)]); ?>
+        </div>
+        <div class="col-auto">
+            <?php render_input(["type" => "select", "id" => "status", "name" => "status", "label" => "Status", "options" => $statuses, "value" => se($search, "status", "", false)]); ?>
+        </div>
+        <div class="col-auto">
+            <?php render_input(["type" => "select", "id" => "breed", "name" => "breed_id", "label" => "Breed", "options" => $breeds, "value" => se($search, "breed_id", "", false)]); ?>
+        </div>
+        <div class="col-auto">
+            <?php render_input(["type" => "select", "id" => "temperament", "name" => "temperament[]", "label" => "Temperament", "options" => $temps, "rules" => ["multiple" => true], "value" => isset($search["temperament"]) ? $search["temperament"] : []]); ?>
+        </div>
+        <div class="col-auto">
+            <?php render_input(["type" => "select", "id" => "sex", "name" => "sex", "label" => "Sex", "options" => $sex, "value" => se($search, "sex", "", false)]); ?>
+        </div>
+        <div class="col-auto">
+            <?php render_input(["type" => "select", "id" => "fixed", "name" => "fixed", "label" => "Fixed (spayed/neutered)", "options" => $fixed, "value" => se($search, "fixed", "", false)]); ?>
+        </div>
+        <div class="col-2">
+            <?php render_input(["type" => "number", "id" => "age_min", "name" => "age_min", "label" => "Age (min)", "value" => se($search, "age_min", "", false)]); ?>
+        </div>
+        <div class="col-2">
+            <?php render_input(["type" => "number", "id" => "age_max", "name" => "age_max", "label" => "Age (max)", "value" => se($search, "age_max", "", false)]); ?>
+        </div>
+        <div class="col-2">
+            <?php render_input(["type" => "select", "id" => "column", "name" => "column", "label" => "Columns", "options" => $cols, "value" => se($search, "column", "", false)]); ?>
+        </div>
+        <div class="col-2">
+            <?php render_input(["type" => "select", "id" => "order", "name" => "order", "label" => "Order", "options" => $orders, "value" => se($search, "order", "", false)]); ?>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-1">
+            <?php render_button(["type" => "submit", "text" => "Search"]); ?>
+        </div>
+        <div class="col-1">
+            <a class="btn btn-secondary" href="?">Reset</a>
+        </div>
+    </div>
+</form>
