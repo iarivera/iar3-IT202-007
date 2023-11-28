95e412f787833a2397ef7a1222d1f3321f4fa747
iarivera <iar3@njit.edu>
2023-11-28 11:55:32 -0500

    update table creation, for new vision

--- public_html/Project/sql/006_create_table_pokemon.sql
+++ public_html/Project/sql/006_create_table_pokemon.sql
@@ -1,10 +1,10 @@
 CREATE TABLE CA_Pokemon(
     `id`    int auto_increment not null,
-    `api_id` int,
+    `api_id` VARCHAR(10),
     `name` VARCHAR(30),
     `type_1` VARCHAR(30),
     `type_2` VARCHAR(30),
-    `captured` BIT(0),
+    `caught` BOOLEAN,
     `created`   timestamp default current_timestamp,
     `modified`  timestamp default current_timestamp on update current_timestamp,
     PRIMARY KEY(`id`),
